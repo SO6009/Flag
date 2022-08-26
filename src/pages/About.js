818a424bb9f31cb7edb54e3591fe30cfa54056cc
So6009 <so6009@gmail.com>
2022-08-26 16:18:10 +0200

    routes and components

---
+++ src/pages/About.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <br /> 
+      <p>
+       Lorem ipsum dolor sit amet. Ad molestias animi sed magnam similique ut magni accusamus rem maxime fugiat eos veritatis
+       sunt vel consequatur eveniet. Est autem facere sed quaerat quisquam quo possimus quos qui provident autem cum nobis nostrum.
+       Aut quasi dolor sit esse eius et molestiae adipisci aut consequatur quas. Est quasi laborum aut laborum sint qui harum blanditiis
+       id praesentium dolor ex dignissimos quibusdam et dolore omnis.
+       Eos pariatur omnis vel velit reiciendis 33 cumque eveniet ut neque minus est alias neque est illum architecto ut vitae impedit?
+       Aut nisi autem in ducimus quis sed commodi tenetur sed quod nihil qui sunt voluptatum a perferendis corrupti!
+       Est sunt nesciunt ut galisum deserunt sed sequi saepe et molestiae quasi quo soluta obcaecati ut molestiae enim sit quasi 
+       libero. Non aperiam vero et numquam necessitatibus sit error inventore. Et molestias galisum qui possimus voluptatum ea
+       voluptatem dolorem.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet. Ad molestias animi sed magnam similique ut magni accusamus rem maxime fugiat eos veritatis sunt vel
+        consequatur eveniet. Est autem facere sed quaerat quisquam quo possimus quos qui provident autem cum nobis nostrum. Aut quasi
+        dolor sit esse eius et molestiae adipisci aut consequatur quas. Est quasi laborum aut laborum sint qui harum blanditiis id
+        praesentium dolor ex dignissimos quibusdam et dolore omnis.
+        Eos pariatur omnis vel velit reiciendis 33 cumque eveniet ut neque minus est alias neque est illum architecto ut vitae impedit?
+        Aut nisi autem in ducimus quis sed commodi tenetur sed quod nihil qui sunt voluptatum a perferendis corrupti!
+        Est sunt nesciunt ut galisum deserunt sed sequi saepe et molestiae quasi quo soluta obcaecati ut molestiae enim sit quasi libero.
+        Non aperiam vero et numquam necessitatibus sit error inventore. Et molestias galisum qui possimus voluptatum ea voluptatem
+        dolorem.
+      </p>
+    </div>
+  );
+};
+
+export default About;
